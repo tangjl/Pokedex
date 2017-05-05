df01f611aa2e376947fd2a84647a90cdf185afcd
rohatgia <rohatgia@vcu.edu>
2017-05-05 15:20:49 -0400

    Search V2

--- pokedex/views.py
+++ pokedex/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, render_to_response
-from .models import Pokemon, Stats
+from .models import Pokemon, Stats, Region, Type
+from .forms import PokemonForm
 from pprint import pprint
 from django.http import HttpResponse 
+from django.db.models import Q
 
 
 def pokemon_list(request):
@@ -10,6 +12,14 @@ def pokemon_list(request):
     return render(request, 'pokedex/pokemon_list.html', {'pokemons': pokemons})
 
 def search(request):
-    query = request.GET.get('q')
-    pokemons = Pokemon.objects.filter(name=query)
+    namer = request.GET.get('q')
+    regionr = request.GET.get('regiontype')
+    climater = request.GET.get('climatetype')
+    typer = request.GET.get('typetype')
+
+    regionid = Region.objects.filter(region_name=regionr)
+    typeid = Type.objects.filter(pokemon_type__contains=typer)
+    pokemons = Pokemon.objects.filter(name=namer,region_id=regionid,climate=climater,type_id=typeid)
+    if len(Pokemon.objects.filter(name=namer,region_id=regionid,climate=climater,type_id=typeid)) == 0:
+        pokemons = Pokemon.objects.order_by('number')
     return render(request, 'pokedex/pokemon_list.html', {'pokemons': pokemons})
