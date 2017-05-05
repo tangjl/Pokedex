59a6a29bd2cd7766e32605aff9bb83f5e35836f4
tangjl <jordanltang@gmail.com>
2017-05-05 01:42:46 -0400

    responsive cards

--- pokedex/views.py
+++ pokedex/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from .models import Pokemon, Stats
-from django.http import HttpResponse 
+
 
 def pokemon_list(request):
     pokemons = Pokemon.objects.order_by('number')
     return render(request, 'pokedex/pokemon_list.html', {'pokemons': pokemons})
-
