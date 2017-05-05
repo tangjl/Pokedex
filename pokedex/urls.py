edb2eeed2e19db67af48e9b6b4d15429307119bb
tangjl <jordanltang@gmail.com>
2017-05-05 05:09:54 -0400

    added button to send back home

--- pokedex/urls.py
+++ pokedex/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     url(r'^$', views.pokemon_list, name='pokemon_list'),
     url(r'^collection', views.pokemon_collection, name='pokemon_collection'),
     url(r'^search', views.search, name='search'),
-    url(r'^search/[?]q=$', views.pokemon_list, name='pokemon_list')
+    url(r'^search/[?]q=$', views.pokemon_list, name='pokemon_search')
 ]
