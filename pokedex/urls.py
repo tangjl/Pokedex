d1e39c67524094536c661a582bac178311e61075
tangjl <jordanltang@gmail.com>
2017-05-05 04:15:31 -0400

    pokemon collections initialized

--- pokedex/urls.py
+++ pokedex/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.pokemon_list, name='pokemon_list'),
+    url(r'^collection', views.pokemon_collection, name='pokemon_collection'),
     url(r'^search', views.search, name='search'),
     url(r'^search/[?]q=$', views.pokemon_list, name='pokemon_list')
 ]
--- pokedex/views.py
+++ pokedex/views.py
@@ -13,3 +13,7 @@ def search(request):
     query = request.GET.get('q')
     pokemons = Pokemon.objects.filter(name=query)
     return render(request, 'pokedex/pokemon_list.html', {'pokemons': pokemons})
+
+def pokemon_collection(request):
+    pokemons = Pokemon.objects.order_by('-number')
+    return render(request, 'pokedex/pokemon_list.html', {'pokemons': pokemons})
