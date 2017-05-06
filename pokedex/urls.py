da38e6e477e361dfa881e3707039e0b1a5b52e2f
tangjl <jordanltang@gmail.com>
2017-05-05 23:22:21 -0400

    documentation and fixed missing sprites

--- pokedex/urls.py
+++ pokedex/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     url(r'^$', views.pokemon_list, name='pokemon_list'),
     url(r'^search', views.search, name='search'),
     url(r'^search/[?]q=$', views.pokemon_list, name='pokemon_search'),
-    url(r'^(?P<username>)', views.pokemon_collection, name='pokemon_collection'),
+    url(r'^user/(?P<username>)', views.pokemon_collection, name='pokemon_collection'),
 ]
