61390b0647f81016be80bf1a53068d4a4026f044
rohatgia <rohatgia@vcu.edu>
2017-05-05 21:37:47 -0400

    SQL Output

--- mysite/settings.py
+++ mysite/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'pokedex.middleware.290.SqlPrintingMiddleware'
 ]
 
 ROOT_URLCONF = 'mysite.urls'
